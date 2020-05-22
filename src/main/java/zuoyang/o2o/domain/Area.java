de574218fd603793b1b9f00cce7a44fc6d14a26c
zuoyangding <zuoyangding@yahoo.com>
2020-05-22 10:47:48 -0700

    add area entity

---
+++ src/main/java/zuoyang/o2o/domain/Area.java
@@ -0,0 +1,16 @@
+package zuoyang.o2o.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class Area {
+    private Integer areaId;
+    private String areaName;
+    private Integer priority;
+    private Date createTime;
+    private Date lastEditTime;
+}
