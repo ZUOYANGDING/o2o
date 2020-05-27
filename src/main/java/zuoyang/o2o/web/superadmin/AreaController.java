095a755c2f0ce44e82d691019435680731a8e602
zuoyangding <zuoyangding@yahoo.com>
2020-05-27 11:18:38 -0700

    add logback config file

--- src/main/java/zuoyang/o2o/web/superadmin/AreaController.java
+++ src/main/java/zuoyang/o2o/web/superadmin/AreaController.java
@@ -1,5 +1,6 @@
 package zuoyang.o2o.web.superadmin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +14,19 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("/superadmin")
-public class SuperAdminController {
+@Slf4j
+public class AreaController {
     private final AreaService areaService;
 
-    public SuperAdminController(AreaService areaService) {
+    public AreaController(AreaService areaService) {
         this.areaService = areaService;
     }
 
     @GetMapping("/listarea")
     @ResponseBody
     public Map<String, Object> listArea() {
+        log.info("====start====");
+        long startTime = System.currentTimeMillis();
         Map<String, Object> modelMap = new HashMap<>();
         try {
             List<Area> areaList = areaService.getAreaList();
@@ -33,6 +37,10 @@ public class SuperAdminController {
             modelMap.put("success", false);
             modelMap.put("errMsg", e.toString());
         }
+        log.error("test error");
+        long endTime = System.currentTimeMillis();
+        log.debug("costTime:[{}ms]", endTime-startTime);
+        log.info("======end======");
         return modelMap;
     }
 
