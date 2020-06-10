e609f3bf84a3139d809c057655c5724881d5d133
zuoyangding <zuoyangding@yahoo.com>
2020-06-10 10:33:21 -0700

    select product category by shopId in DAO layer

---
+++ src/main/java/zuoyang/o2o/dao/ProductCategoryDao.java
@@ -0,0 +1,9 @@
+package zuoyang.o2o.dao;
+
+import zuoyang.o2o.entity.ProductCategory;
+
+import java.util.List;
+
+public interface ProductCategoryDao {
+    List<ProductCategory> queryProductCategory(Long shopId);
+}
