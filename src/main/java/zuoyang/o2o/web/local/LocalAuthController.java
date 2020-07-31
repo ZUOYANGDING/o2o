c346edcb32102011e2921f8885bb292fb45c32ca
zuoyangding <zuoyangding@yahoo.com>
2020-07-31 10:41:32 +0800

    complete localauth at controller layer without test

--- src/main/java/zuoyang/o2o/web/local/LocalAuthController.java
+++ src/main/java/zuoyang/o2o/web/local/LocalAuthController.java
@@ -12,6 +12,7 @@ import zuoyang.o2o.exception.LocalAuthOperationException;
 import zuoyang.o2o.service.LocalAuthService;
 import zuoyang.o2o.util.CodeUtil;
 import zuoyang.o2o.util.HttpServletRequestUtil;
+import zuoyang.o2o.util.MD5Util;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -69,5 +70,82 @@ public class LocalAuthController {
         return modelMap;
     }
 
-    
+    @PostMapping("/changelocalpwd")
+    @ResponseBody
+    private Map<String, Object> updatePassword(HttpServletRequest request)  {
+        Map<String, Object> modelMap = new HashMap<>();
+        if (!CodeUtil.checkVerifyCode(request)) {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "Invalid verification code");
+            return modelMap;
+        }
+
+        String username = HttpServletRequestUtil.getString(request, "username");
+        String password = HttpServletRequestUtil.getString(request, "password");
+        String newPassword = HttpServletRequestUtil.getString(request, "newPassword");
+        PersonInfo personInfo = (PersonInfo) request.getSession().getAttribute("user");
+        if (username!=null && password!=null && newPassword!=null && personInfo!=null
+                && personInfo.getUserId()!=null && !password.equals(newPassword)) {
+            LocalAuth tempLocalAuth = null;
+            long userId = personInfo.getUserId();
+            try {
+                tempLocalAuth = localAuthService.getLocalAuthByUserId(userId);
+            } catch (LocalAuthOperationException e) {
+                modelMap.put("success", false);
+                modelMap.put("errMsg", e.getMessage());
+                return modelMap;
+            }
+            if (tempLocalAuth==null || !username.equals(tempLocalAuth.getUsername()) ||
+                    !MD5Util.encryptByMD5(password).equals(tempLocalAuth.getPassword())) {
+                modelMap.put("success", false);
+                modelMap.put("errMsg", "UserName or Password is invalid");
+                return modelMap;
+            }
+
+            try {
+                LocalAuthExecution localAuthExecution = localAuthService.updateLocalAuth(userId, username, password,
+                        newPassword);
+                if (localAuthExecution.getState() == LocalAuthStateEnum.SUCCESS.getState()) {
+                    modelMap.put("success", true);
+                } else if (localAuthExecution.getState() == LocalAuthStateEnum.INVALID_ARGS.getState()) {
+                    modelMap.put("success", false);
+                    modelMap.put("errMsg", LocalAuthStateEnum.INVALID_ARGS.getStateInfo());
+                } else {
+                    modelMap.put("success", false);
+                    modelMap.put("errMsg", LocalAuthStateEnum.MISSING_ARGS.getStateInfo());
+                }
+            } catch (LocalAuthOperationException e) {
+                modelMap.put("success", false);
+                modelMap.put("errMsg", e.getMessage());
+            }
+            return modelMap;
+        } else {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "Miss required arguments");
+            return modelMap;
+        }
+    }
+
+    @PostMapping("/logincheck")
+    @ResponseBody
+    private Map<String, Object> checkLoginStatus(HttpServletRequest request) {
+        Map<String, Object> modelMap = new HashMap<>();
+        boolean needVerify = HttpServletRequestUtil.getBoolean(request, "needVerify");
+        if (needVerify && !CodeUtil.checkVerifyCode(request)) {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "Invalid verification code");
+            return modelMap;
+        }
+
+        String username = HttpServletRequestUtil.getString(request, "username");
+        String password = HttpServletRequestUtil.getString(request, "password");
+
+        if (username!=null && password!=null) {
+            try {
+                LocalAuth localAuth = new LocalAuth();
+                localAuth.setUsername(username);
+                localAuth.setPassword(password);
+            }
+        }
+    }
 }
